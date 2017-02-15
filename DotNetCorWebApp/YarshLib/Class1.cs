0a042c4210c38341de0c2c696601d9319eb06989
Josh Smith <joshsmithdev@gmail.com>
2017-02-15 15:24:25 -0700

    Introduction to ASP.NET Core 1.0

---
+++ DotNetCorWebApp/YarshLib/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace YarshLib
+{
+    public class Class1
+    {
+        public static string Greeting()
+        {
+            return "Hello from YarshLib!";
+        }
+    }
+}
